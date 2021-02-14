7c5b552595013c3c4cea225f02b4a611699485b4
BabisVas <79044879+BabisVas@users.noreply.github.com>
2021-02-14 23:58:14 +0200

    Add files via upload

---
+++ kpos.py
@@ -0,0 +1,21 @@
+import pandas as pd  
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from pandas import DataFrame
+import nltk
+
+
+df = pd.read_csv("pos3.csv" )
+print(df.head())
+b = range(0,2)
+a = []
+for i in b:
+    wrd = df.iloc[i,1]
+    token = nltk.word_tokenize(wrd)
+    a.append(token)
+    if i == 2:
+        break
+    
+print(a)    
+    
