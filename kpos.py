a44d856530b91e67d86d4a07c89f4badc6a89609
BabisVas <79044879+BabisVas@users.noreply.github.com>
2021-02-17 14:10:55 +0200

    Add files via upload
    allaksa to for loop se while kai ta onomata stis listes gia na fainetai pio oraio

--- kpos.py
+++ kpos.py
@@ -1,21 +1,34 @@
+import pandas as pd  
+from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
-import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from pandas import DataFrame
-import string
 import nltk
+from nltk.corpus import stopwords 
+ 
 
 
 df = pd.read_csv("pos3.csv" )
+print(df.head())
 b = range(0,2)
-a = []
-c=[]
-for i in b:
-    wrd = df.iloc[i,1]
-    wrd_no_punctuation = wrd.translate(str.maketrans('', '', string.punctuation))#bgazei ta shmeia stikshs
-    tokenlist = nltk.word_tokenize(wrd_no_punctuation)
-    a.append(tokenlist)
-    c.append(len(tokenlist))
+sentences = []
+length = []
+filtered = []
+sentiments = []
+stop_words = set(stopwords.words('english'))
 
-print(a)   
-print(c)
+i = 0
+while i < 2:
+    wrd = df.iloc[i,1]
+    token = nltk.word_tokenize(wrd)
+    for w in token:
+        if w not in stop_words:
+            filtered.append(w)
+    sentences.append(filtered)
+    length.append(len(filtered))                
+    i += 1
+    
+print(sentences)    
+print(length)
+ 
+
