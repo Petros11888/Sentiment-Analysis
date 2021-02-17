471b2434f41bf359714d562a72d3f48cc1a108f3
BabisVas <79044879+BabisVas@users.noreply.github.com>
2021-02-17 18:54:26 +0200

    Add files via upload
    ekana to lemmatization kai evgala ta http

--- kpos.py
+++ kpos.py
@@ -5,30 +5,31 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from pandas import DataFrame
 import nltk
 from nltk.corpus import stopwords 
- 
-
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk import word_tokenize
+import re
 
 df = pd.read_csv("pos3.csv" )
 print(df.head())
-b = range(0,2)
 sentences = []
 length = []
-filtered = []
-sentiments = []
 stop_words = set(stopwords.words('english'))
-
 i = 0
-while i < 2:
+lmtzr = WordNetLemmatizer()
+while i <= 1:
     wrd = df.iloc[i,1]
-    token = nltk.word_tokenize(wrd)
-    for w in token:
-        if w not in stop_words:
-            filtered.append(w)
-    sentences.append(filtered)
-    length.append(len(filtered))                
+    t1 = re.sub(r'http\S+', '', wrd)
+    token = nltk.word_tokenize(t1)
+    token = [word.lower() for word in token if word.isalpha()]
+    filtered_words = [word for word in token if word not in stopwords.words('english')]
+    lemmatized = [[lmtzr.lemmatize(word) for word in word_tokenize(s)]
+              for s in filtered_words]
+    sentences.append(filtered_words)
+    length.append(len(filtered_words))                
     i += 1
+  
+  
     
 print(sentences)    
 print(length)
  
-
