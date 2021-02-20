34ea65bc61b60ff882884f4331cb3711dd2f0059
Petros11888 <Petros11888@gmail.com>
2021-02-20 14:53:27 +0200

    pio grhgoro search, pros8esa na afairei kapoia pramatakia akoma kai ebala ena onoma gia tis listes

--- first_try_nbayes.py
+++ first_try_nbayes.py
@@ -1,16 +1,19 @@
-import pandas as pd  
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from pandas import DataFrame
+import pandas as pd  
 import nltk
+import numpy as np
 from nltk.corpus import stopwords 
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
 from nltk.stem import PorterStemmer
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 #names=['id', 'text', 'sentiment']
 df = pd.read_csv("all_data.csv", )
 
@@ -32,30 +35,20 @@ for i in range(0,len(sentences)):
     token_list = nltk.word_tokenize(t)
     token_list = [word.lower() for word in token_list if word.isalpha()]
     token_list = [word for word in token_list if word not in stopwords.words('english')]
-    token_list = [lmtzr.lemmatize(token) for token in token_list]
-    #token_list = [stemmer.stem(token) for token in token_list]       
+    #token_list = [lmtzr.lemmatize(token) for token in token_list]
+    token_list = [stemmer.stem(token) for token in token_list]       
     together.append(separator.join(token_list))
 
 vectorizer = TfidfVectorizer()
+
 X = vectorizer.fit_transform(together)
 y = df.iloc[:, 2].values
 
-
 X_train, X_test, y_train, y_test = train_test_split(X.toarray(), y, test_size=0.20)
 
-scaler = StandardScaler()
-scaler.fit(X_train)
+model = MultinomialNB().fit(X_train, y_train)
 
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-
-
-classifier = KNeighborsClassifier(n_neighbors=30)
-classifier.fit(X_train, y_train)
-
-y_pred = classifier.predict(X_test)
-
-
+y_pred = model.predict(X_test)
 
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred)) 
--- first_try_svm.py
+++ first_try_svm.py
@@ -1,10 +1,10 @@
 import pandas as pd  
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from pandas import DataFrame
 import nltk
+from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords 
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
@@ -16,39 +16,41 @@ from sklearn.svm import SVC
 
 df = pd.read_csv("all_data.csv", )
 
-sentences = []
+sentences = df.iloc[:, 1].values
 length = []
 together=[]
 separator = ' '
 stop_words = set(stopwords.words('english'))
-i = 0
+stemmer = PorterStemmer()
 lmtzr = WordNetLemmatizer()
-for i in range(0,len(df.index)):
-    wrd = df.iloc[i,1]
-    t1 = re.sub(r'http\S+', '', wrd)
-    t2 = re.sub(r'@\S+', '', t1)
-    token_list = nltk.word_tokenize(t2)
-    low_token_list = [word.lower() for word in token_list if word.isalpha()]
-    filtered_words = [word for word in low_token_list if word not in stopwords.words('english')]
-    lemmatized = [lmtzr.lemmatize(s) for s in filtered_words]     
-    together.append(separator.join(lemmatized))
+for i in range(0,len(sentences)):
+    t = sentences[i]
+    t = re.sub(r'\s+[a-zA-Z]\s+', ' ', t)
+    t = re.sub(r'http\S+', '', t)
+    t = re.sub(r'@\S+', '', t)
+    t = re.sub(r'#\S+', '', t)
+    t = re.sub(r'\s+', ' ', t, flags=re.I)
+    token_list = nltk.word_tokenize(t)
+    token_list = [word.lower() for word in token_list if word.isalpha()]
+    token_list = [word for word in token_list if word not in stopwords.words('english')]
+    token_list = [lmtzr.lemmatize(token) for token in token_list]
+    #token_list = [stemmer.stem(token) for token in token_list]       
+    together.append(separator.join(token_list))
 
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(together)
 
 y = df.iloc[:, 2].values
 
-
 X_train, X_test, y_train, y_test = train_test_split(X.toarray(), y, test_size=0.20)
 
-
 svclassifier = SVC(kernel='linear')
+#svclassifier = SVC(kernel='rbf')
+#svclassifier = SVC(kernel='poly', degree=3)
+#svclassifier = SVC(kernel='sigmoid')
 svclassifier.fit(X_train, y_train)
 
-
 y_pred = svclassifier.predict(X_test)
-
-
 
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred)) 
